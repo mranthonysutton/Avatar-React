c730eddf979d4580143cf8d7a69b0a4c1bcf7996
Anthony Sutton <anthony.sutton95@gmail.com>
2020-05-04 10:52:48 -0700

    Data from API call is rendering character cards :sparkles:

--- src/components/character/RenderCharacter.js
+++ src/components/character/RenderCharacter.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axiosHook from "../../utils/axiosHook";
 
 const RenderCharacter = () => {
+  const [characterData, setCharacterData] = useState({});
+
+  useEffect(() => {
+    axiosHook()
+      .get("/api/v1/characters")
+      .then((response) => {
+        setCharacterData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="char-card">
-      <h3>Character Name</h3>
-      <p>Character Affiliation</p>
-      <p>Char image</p>
-      <div className="character-buttons">
-        <button>Allies</button>
-        <button>Enemies</button>
-      </div>
+      {characterData.length > 0 &&
+        characterData.map((character) => (
+          <div className="char-card" key={character._id}>
+            <h3>{character.name}</h3>
+            <p>{character.affiliation}</p>
+            <img src={character.photoUrl} alt={character.name} />
+            <div className="character-buttons">
+              <button>Allies</button>
+              <button>Enemies</button>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
 
 export default RenderCharacter;
-
--- src/components/character/character.js
+++ src/components/character/character.js
@@ -1,24 +1,10 @@
-import React, { useEffect, useState } from "react";
-import axiosHook from "../../utils/axiosHook";
+import React from "react";
 import RenderCharacter from "./RenderCharacter";
 
 const Character = () => {
-  const [characterData, setCharacterData] = useState({});
-
-  useEffect(() => {
-    axiosHook()
-      .get("/api/v1/characters")
-      .then((response) => {
-        setCharacterData(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
   return (
     <div>
-      <h3>Avatar Characters</h3>
+      <h1>Avatar Characters</h1>
       <RenderCharacter />
     </div>
   );
